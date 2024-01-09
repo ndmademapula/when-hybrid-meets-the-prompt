a49fb3d78d7206d5310b9a332521b15a467ecaec
ndmademapula <29minh11anh03@gmail.com>
2024-01-09 15:37:07 +0700

    (；′⌒`)

--- Model/ultils.py
+++ Model/ultils.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly.graph_objects as go
-# from Model.Hybrid.Rec_Hybrid import Recommender
 
 df_info = pd.read_csv('Data/courses_list.csv')
 df_courses = pd.read_csv('Data/courses.csv')
